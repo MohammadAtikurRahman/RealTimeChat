16016c3f4163d7fc58627e18e8cff04d0f45ac22
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2022-08-30 17:01:41 +0600

    CreatSigninpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-
+import Signin from './components/Signin';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-         
+         <Signin /> 
       </header>
     </div>
   );
---
+++ src/components/Signin.jsx
@@ -0,0 +1,42 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+function Signin() {
+  return (
+
+    <Card style={{ width: '28rem' }}>
+        <Card.Body>
+                            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email"   placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" style={{fontSize: '16px'}} label="Remember me" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Sign In 
+                </Button>
+                </Form>
+
+
+
+        </Card.Body>
+
+ 
+
+
+    </Card>
+  
+  );
+}
+
+export default Signin;
